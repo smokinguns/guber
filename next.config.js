5cc3dddacce26aeb0fae1daafaa0fb2da74cbaf7
Bradley Lower <bradleylower@Bradleys-MacBook-Pro.local>
2023-04-23 08:08:05 -0500

    fix

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output:'export',
-  assetPrefix: isProd ? '/guber' : undefined,
+  assetPrefix: isProd ? 'https://smokinguns.github.io/guber' : undefined,
   experimental: {
     appDir: true,
   },
