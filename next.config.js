6a75a0b7b03eedb670c1348eab964fb9f75d80ac
smokinguns <distortb@hotmail.com>
2023-04-23 07:25:24 -0500

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-<<<<<<< HEAD
   output:'export',
-=======
-  output: 'export',
->>>>>>> 9c86a3b6517fc06c379fc57d37aba7669e1da581
   experimental: {
     appDir: true,
   },
