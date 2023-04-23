383f95e8e29eed8164db834da4d96597b5001afe
Bradley Lower <bradleylower@Bradleys-MacBook-Pro.local>
2023-04-23 08:01:48 -0500

    latest

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   output:'export',
-  assetPrefix: isProd ? 'guber' : undefined,
+  assetPrefix: isProd ? '/guber' : undefined,
   experimental: {
     appDir: true,
   },
