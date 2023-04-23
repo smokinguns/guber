45adf6b511dfff4f42453670c975d53998b601b7
Bradley Lower <bradleylower@Bradleys-MacBook-Pro.local>
2023-04-23 14:59:31 -0500

    add label on simple

--- src/lib/components/host-container.tsx
+++ src/lib/components/host-container.tsx
@@ -5,7 +5,7 @@ import Player from './player';
 export default function HostContainer(props: any) {
     const [videoId, setVideoId] = useState("");
     return <div>
-        <TextField value={videoId} onChange={(event)=>{
+        <TextField label="Youtube ID (2DwZzHimUlU)" value={videoId} onChange={(event)=>{
             setVideoId(event.target.value);
         }}></TextField>
        {videoId.length >0 ?<Player videoId={videoId}></Player> : <></>}
