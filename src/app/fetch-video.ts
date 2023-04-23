d200f9d42e63d26e872a71b7862282f456f1f4d7
Bradley Lower <bradleylower@Bradleys-MacBook-Pro.local>
2023-04-23 08:46:53 -0500

    latest

--- src/app/fetch-video.ts
+++ src/app/fetch-video.ts
@@ -2,7 +2,7 @@
 export async function getVidoeData(videoId:string){
 
    
-    let resp = await fetch(`http://0.0.0.0:3000/api/video/${videoId}`,{ next: { revalidate: 10 }});
+    let resp = await fetch(`http://localhost:8888/api/video/${videoId}`,{ next: { revalidate: 10 }});
     
 
 
